18e89199c3305cf177e89f9406d31ff818ee671d
Berserk9000 <cruz.d.kevin@gmail.com>
2022-09-09 17:10:16 -0400

    setting up products apis 'get' working and displaying :)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+
+
+router.get('/', async (req, res) => {
+  try {
+    // find all categories with its associated Products
+    const categoryData = await Category.findAll({
+      include: [
+        {model: Product}
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }  
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    // find one category by its `id` value and its associated Products 
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product
+      }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
